dabb59bcce06568ca97b4bd381e5f88d5e167a73
Adam Fabicki <adam@fabicki.com>
2023-10-19 21:54:45 -0400

    Updated tests to work in SWI-Prolog version 7+

--- prolog/bencode.pl
+++ prolog/bencode.pl
@@ -45,47 +45,48 @@ keys_sorted(L) :-
 :- begin_tests(bencode).
 test(spam_encode) :-
     bencode(spam, X),
-    X = "4:spam".
+    X = `4:spam`.
 test(spam_decode) :-
-    bencode(X, "4:spam"),
+    bencode(X, `4:spam`),
     X = spam.
+
 % TODO test arbitrary byte strings
 
 test(int_encode) :-
     bencode(42, X),
-    X = "i42e".
+    X = `i42e`.
 test(int_decode) :-
-    bencode(X, "i42e"),
+    bencode(X, `i42e`),
     X = 42.
 
 test(negative_encode) :-
     bencode(-3, X),
-    X = "i-3e".
+    X = `i-3e`.
 test(negative_decode) :-
-    bencode(X, "i-9e"),
+    bencode(X, `i-9e`),
     X = -9.
 
 test(list_encode) :-
     bencode([spam, eggs], X),
-    X = "l4:spam4:eggse".
+    X = `l4:spam4:eggse`.
 test(list_decode) :-
-    bencode(X, "l4:spam4:eggse"),
+    bencode(X, `l4:spam4:eggse`),
     X = [spam, eggs].
 
 test(dictionary_encode) :-
     bencode([cow-moo, spam-eggs], X),
-    X = "d3:cow3:moo4:spam4:eggse".
+    X = `d3:cow3:moo4:spam4:eggse`.
 test(dictionary_decode) :-
-    bencode(X, "d3:cow3:moo4:spam4:eggse"),
+    bencode(X, `d3:cow3:moo4:spam4:eggse`),
     X = [cow-moo, spam-eggs].
 test(dictionary_list_value) :-
-    bencode([spam-[a,b]], "d4:spaml1:a1:bee").
+    bencode([spam-[a,b]], `d4:spaml1:a1:bee`).
 test(dictionary_longer) :-
     bencode([ publisher-bob
             , 'publisher-webpage'-'www.example.com'
             , 'publisher.location'-home
             ],
-            "d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee"
+            `d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee`
            ).
 
 :- end_tests(bencode).
